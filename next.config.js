eaa1de9c3cdde109cf8333e0be6064b7747489ad
Muhammad Ghalep <mghalep8@gmail.com>
2026-05-12 04:58:28 +0300

    include remotion packages in lambda bundle

--- next.config.js
+++ next.config.js
@@ -2,13 +2,21 @@
 
 const nextConfig = {
   reactStrictMode: false,
-
   swcMinify: true,
 
   images: {
     unoptimized: true,
     loader: "custom",
     loaderFile: "./image-loader.js",
+  },
+
+  outputFileTracingIncludes: {
+    "/api/render": [
+      "./node_modules/@remotion/bundler/**/*",
+      "./node_modules/@remotion/renderer/**/*",
+      "./node_modules/@remotion/lambda/**/*",
+      "./node_modules/remotion/**/*",
+    ],
   },
 };
 
